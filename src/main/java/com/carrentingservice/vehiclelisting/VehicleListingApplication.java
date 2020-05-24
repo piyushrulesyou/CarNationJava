87c78d093ef3ce8556e1b3b5ce084887b5ddef43
Piyush <piyush.agarwal01@nagarro.com>
2020-05-25 00:09:09 +0530

    inital commit

---
+++ src/main/java/com/carrentingservice/vehiclelisting/VehicleListingApplication.java
@@ -0,0 +1,13 @@
+package com.carrentingservice.vehiclelisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleListingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleListingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carrentingservice/vehiclelisting/VehicleListingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrentingservice.vehiclelisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleListingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
