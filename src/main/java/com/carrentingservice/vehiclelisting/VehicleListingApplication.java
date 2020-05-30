743327a75383f1b29a5c4df755c399dcbf97266b
Piyush <piyush.agarwal01@nagarro.com>
2020-05-30 16:38:53 +0530

    updated main class SpringBoot

--- src/main/java/com/carrentingservice/vehiclelisting/VehicleListingApplication.java
+++ src/main/java/com/carrentingservice/vehiclelisting/VehicleListingApplication.java
@@ -2,9 +2,16 @@ package com.carrentingservice.vehiclelisting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VehicleListingApplication {
+public class VehicleListingApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VehicleListingApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VehicleListingApplication.class, args);
