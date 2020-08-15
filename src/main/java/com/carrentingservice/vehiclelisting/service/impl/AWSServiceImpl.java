e30459ccf0257a5b99fb170a0354e8bd725d4416
Piyush <piyush.agarwal01@nagarro.com>
2020-08-16 01:26:56 +0530

    added logger

--- src/main/java/com/carrentingservice/vehiclelisting/service/impl/AWSServiceImpl.java
+++ src/main/java/com/carrentingservice/vehiclelisting/service/impl/AWSServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,6 +28,8 @@ import com.carrentingservice.vehiclelisting.service.AWSService;
 
 @Service
 public class AWSServiceImpl implements AWSService {
+
+	final static Logger LOGGER = Logger.getLogger(AWSServiceImpl.class);
 
 	private AmazonS3 s3Client;
 
@@ -48,30 +51,31 @@ public class AWSServiceImpl implements AWSService {
 	@Override
 	public List<String> uploadFilesToS3(List<MultipartFile> carImages) throws IOException {
 		List<String> carImagesURL = new ArrayList<>();
-		System.out.println("**************11111111111111");
+		LOGGER.info("**************11111111111111");
 		try {
-			System.out.println("------------------2222222222222222");
+			LOGGER.info("------------------2222222222222222");
 			for (MultipartFile multipartFile : carImages) {
-				System.out.println("------------------333333333333333333333");
+				LOGGER.info("------------------333333333333333333333");
 				String imageURL = "";
-				System.out.println("------------------444444444444444444444");
+				LOGGER.info("------------------444444444444444444444");
 				File file = convertMultipartFileToFile(multipartFile);
-				System.out.println("------------------555555555555555555");
-				System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"+file.getName());
+				LOGGER.info("------------------555555555555555555");
+				LOGGER.info("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" + file.getName());
 				String fileName = generateFileName(multipartFile);
-				System.out.println("()()()()()()()()() " + fileName);
-				System.out.println("------------------66666666666666666666666666");
-				System.out.println("------------------" + commonConfiguration.getS3BucketName());
-				PutObjectResult por = s3Client.putObject(new PutObjectRequest(commonConfiguration.getS3BucketName(), fileName, file)
-						.withCannedAcl(CannedAccessControlList.PublicRead));
-				System.out.println(por);
-				System.out.println("------------------7777777777777777777777777");
+				LOGGER.info("()()()()()()()()() " + fileName);
+				LOGGER.info("------------------66666666666666666666666666");
+				LOGGER.info("------------------" + commonConfiguration.getS3BucketName());
+				PutObjectResult por = s3Client
+						.putObject(new PutObjectRequest(commonConfiguration.getS3BucketName(), fileName, file)
+								.withCannedAcl(CannedAccessControlList.PublicRead));
+				LOGGER.info(por);
+				LOGGER.info("------------------7777777777777777777777777");
 				imageURL = commonConfiguration.getS3BucketURL() + fileName;
-				System.out.println("------------------88888888888888888888888888888888888");
+				LOGGER.info("------------------88888888888888888888888888888888888");
 				file.delete();
-				System.out.println("------------------99999999999999999999999999999999");
+				LOGGER.info("------------------99999999999999999999999999999999");
 				carImagesURL.add(imageURL);
-				System.out.println("------------------10101010101010101001001");
+				LOGGER.info("------------------10101010101010101001001");
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
